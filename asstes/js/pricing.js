f567ea07e4581ecc3d7117aa216e50cb97bf4c4e
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2025-01-27 01:16:49 +0600

    JS file update

--- asstes/js/pricing.js
+++ asstes/js/pricing.js
@@ -1,22 +1,22 @@
 // -------------------PRICING SECTION START-----------------
-// let monthlyPlan = document.querySelector("#monthly");
-// let yearlyPlan = document.querySelector("#yearly");
-// let yearlyPlanSection = document.querySelector("#yearly_section")
-// let monthlyPlanSection = document.querySelector("#monthly_section")
+let monthlyPlan = document.querySelector("#monthly");
+let yearlyPlan = document.querySelector("#yearly");
+let yearlyPlanSection = document.querySelector("#yearly_section")
+let monthlyPlanSection = document.querySelector("#monthly_section")
 
-// yearlyPlan.addEventListener("click", function () {
-//     monthlyPlan.classList.add("monthly_active");
-//     yearlyPlan.classList.add("yearly_active");
-//     yearlyPlanSection.classList.add("tk");
-//     monthlyPlanSection.classList.add("monthly_dic")
-// });
+yearlyPlan.addEventListener("click", function () {
+    monthlyPlan.classList.add("monthly_active");
+    yearlyPlan.classList.add("yearly_active");
+    yearlyPlanSection.classList.add("tk");
+    monthlyPlanSection.classList.add("monthly_dic")
+});
 
-// monthlyPlan.addEventListener("click", function () {
-//     monthlyPlan.classList.remove("monthly_active");
-//     yearlyPlan.classList.remove("yearly_active");
-//     yearlyPlanSection.classList.remove("tk");
-//     monthlyPlanSection.classList.remove("monthly_dic")
-// });
+monthlyPlan.addEventListener("click", function () {
+    monthlyPlan.classList.remove("monthly_active");
+    yearlyPlan.classList.remove("yearly_active");
+    yearlyPlanSection.classList.remove("tk");
+    monthlyPlanSection.classList.remove("monthly_dic")
+});
 
 
 // -------------------PRICING SECTION END-----------------
