9ca5cfc940928121cb62f57913f83fdf4134e564
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2025-01-27 01:03:57 +0600

    Initial commit

---
+++ asstes/js/app.js
@@ -0,0 +1,210 @@
+let menuBtn = document.querySelector(".menu_btn");
+let menuBar = document.querySelector(".off_canvas");
+let crossBtn = document.querySelector(".cross_btn");
+
+menuBtn.addEventListener("click", function(){
+    menuBar.classList.add("active_offcanvas");
+    // alert("Assalamu alaikum")
+});
+
+crossBtn.addEventListener("click", function(){
+    menuBar.classList.remove("active_offcanvas");
+})
+
+
+// ----------SEARCH START---------------
+let searchBtn = document.querySelector(".search");
+let searchBar = document.querySelector("#search_bar")
+let searchCross = document.querySelector(".search_cross");
+let menuSearch = document.querySelector(".menu_search");
+
+searchBtn.addEventListener("click", function(){
+    searchBar.classList.add("active_search");
+});
+
+menuSearch.addEventListener("click", function(){
+    searchBar.classList.add("active_search")
+})
+searchCross.addEventListener("click", function(){
+    searchBar.classList.remove("active_search");
+});
+
+
+
+
+// ----------SEARCH END---------------
+
+// -----------PORTFOLIO START---------
+let firstImage = document.querySelector(".hover_img");
+let firstImage2 = document.querySelector(".hover_img2");
+let firstImage3 = document.querySelector(".hover_img3");
+let hoverHere = document.querySelector(".card_one");
+let hoverHere2 = document.querySelector(".card_2");
+let hoverHere3 = document.querySelector(".card_3");
+
+hoverHere.addEventListener("mouseover", function(){
+    firstImage.src = "./asstes/images/portfolio/Vector2.png"
+});
+hoverHere.addEventListener("mouseout", function(){
+    firstImage.src = "./asstes/images/portfolio/Vector.png"
+});
+
+hoverHere2.addEventListener("mouseover", function(){
+    firstImage2.src = "./asstes/images/portfolio/Vector2.png"
+});
+hoverHere2.addEventListener("mouseout", function(){
+    firstImage2.src = "./asstes/images/portfolio/Vector.png"
+});
+
+hoverHere3.addEventListener("mouseover", function(){
+    firstImage3.src = "./asstes/images/portfolio/Vector2.png"
+});
+hoverHere3.addEventListener("mouseout", function(){
+    firstImage3.src = "./asstes/images/portfolio/Vector.png"
+});
+// -----------PORTFOLIO END---------
+
+// ---------------------WHY CHOOSE US START------------
+// let showOne = document.querySelector(".show");
+// let hideOne = document.querySelector(".hide");
+// let mainBody = document.querySelector(".child_para");
+// let mainHeight = document.querySelector(".one");
+
+// hideOne.addEventListener("click", function () {
+//     show.classList.add("kioum");
+// });
+// showOne.addEventListener("click", function () {
+//     show.classList.remove("active_show");
+
+// });
+
+
+
+
+let hideButtons = document.querySelectorAll(".hide");
+let showButtons = document.querySelectorAll(".show");
+
+
+hideButtons.forEach((hideButton) => {
+    hideButton.addEventListener("click", function () {
+        let parent = hideButton.closest(".one");
+        let mainBody = parent.querySelector(".child_para");
+        let showButton = parent.querySelector(".show");
+
+        showButton.classList.add("active_show");
+        mainBody.classList.add("body_active");
+        parent.classList.add("new_height");
+        hideButton.classList.add("active_hide")
+    });
+});
+
+showButtons.forEach((showButton) => {
+    showButton.addEventListener("click", function () {
+        let parent = showButton.closest(".one");
+        let mainBody = parent.querySelector(".child_para");
+        let hideButton = parent.querySelector(".hide");
+
+        showButton.classList.remove("active_show");
+        mainBody.classList.remove("body_active");
+        parent.classList.remove("new_height");
+        hideButton.classList.remove("active_hide")
+    });
+});
+
+// ---------------------WHY CHOOSE US END------------
+
+
+
+
+// JQUERY START 
+
+// ------GREAT TEAM--------
+
+$(function () {
+$('.parent').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+    nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
+    responsive: [
+        // Laptop
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1, 
+                slidesToScroll: 1,
+                // dots: true,
+            }
+        },
+        
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1, 
+                slidesToScroll: 1,
+                arrows: false, 
+            }
+        },
+  
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false, 
+            }
+        }
+    ]
+});
+
+$('.blog_and_news').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+    nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
+    responsive: [
+        // Laptop
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1, 
+                slidesToScroll: 1,
+                // dots: true,
+            }
+        },
+        
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1, 
+                slidesToScroll: 1,
+                arrows: false, 
+            }
+        },
+  
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false, 
+            }
+        }
+    ]
+});
+
+
+
+    $('.tklove').slick({
+        dots: true,
+        arrows: true,
+        dotsClass: 'container docts-active',
+        autoplay: true,
+        autoplaySpeed: 1000,
+        prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
+        nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
+    });
+
+});
---
+++ asstes/js/pricing.js
@@ -0,0 +1,55 @@
+// -------------------PRICING SECTION START-----------------
+// let monthlyPlan = document.querySelector("#monthly");
+// let yearlyPlan = document.querySelector("#yearly");
+// let yearlyPlanSection = document.querySelector("#yearly_section")
+// let monthlyPlanSection = document.querySelector("#monthly_section")
+
+// yearlyPlan.addEventListener("click", function () {
+//     monthlyPlan.classList.add("monthly_active");
+//     yearlyPlan.classList.add("yearly_active");
+//     yearlyPlanSection.classList.add("tk");
+//     monthlyPlanSection.classList.add("monthly_dic")
+// });
+
+// monthlyPlan.addEventListener("click", function () {
+//     monthlyPlan.classList.remove("monthly_active");
+//     yearlyPlan.classList.remove("yearly_active");
+//     yearlyPlanSection.classList.remove("tk");
+//     monthlyPlanSection.classList.remove("monthly_dic")
+// });
+
+
+// -------------------PRICING SECTION END-----------------
+
+let summeryOne = document.querySelector(".one");
+let summeryTow = document.querySelector(".tow");
+let summeryThree = document.querySelector(".three");
+let bodyOne = document.querySelector(".body_one");
+let bodyTow = document.querySelector(".body_tow");
+let bodyThree = document.querySelector(".body_three");
+
+summeryOne.addEventListener("click", function () {
+    bodyTow.classList.remove("body_tow_active");
+    bodyOne.classList.remove("body_one_remove");
+    bodyThree.classList.remove("body_three_active");
+    summeryOne.classList.remove("name_remove_bg"); //one is active
+    summeryTow.classList.remove("name_add_bg"); //tow is remove
+    summeryThree.classList.remove("name_add_bg"); //three is remove
+});
+summeryTow.addEventListener("click", function () {
+    bodyTow.classList.add("body_tow_active");
+    bodyOne.classList.add("body_one_remove");
+    bodyThree.classList.remove("body_three_active");
+    summeryTow.classList.add("name_add_bg"); //tow is add
+    summeryOne.classList.add("name_remove_bg"); //one is remove
+    summeryThree.classList.remove("name_add_bg"); //three is remove
+});
+summeryThree.addEventListener("click", function () {
+    bodyTow.classList.remove("body_tow_active");
+    bodyOne.classList.add("body_one_remove");
+    bodyThree.classList.add("body_three_active");
+    summeryThree.classList.add("name_add_bg");  //three is active
+    summeryOne.classList.add("name_remove_bg"); //one is remoe
+    summeryTow.classList.remove("name_add_bg"); //tow is remove
+
+});
